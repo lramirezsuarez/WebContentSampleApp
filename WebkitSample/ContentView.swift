21f0238d5771e27ca4aca9cc7299581b1868f62b
Luis Ramirez <lramirezsuarez@gmail.com>
2025-01-20 18:23:49 -0500

    Initial Commit

---
+++ WebkitSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebkitSample
+//
+//  Created by Luis Alejandro Ramirez Suarez on 20/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebkitSample/WebkitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebkitSampleApp.swift
+//  WebkitSample
+//
+//  Created by Luis Alejandro Ramirez Suarez on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct WebkitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
